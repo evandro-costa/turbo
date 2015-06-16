773cab4b89cb68d625ba90b2225c228a3d6dd694
John Abrahamsen <jhnabrhmsn@gmail.com>
2015-06-16 21:20:39 +0200

    Cleaned up install.bat a little bit.

--- install.bat
+++ install.bat
@@ -1,41 +1,57 @@
 SET CURRENT_PATH_PREINSTALL=%CD%
-setx PATH "%PATH%;C:\turbo.lua;C:\turbo.lua\luarocks\2.2" /M
+SET TURBO_ROOT=C:\turbo.lua
+SET TURBO_LUAROCKS=%TURBO_ROOT%\luarocks
+SET TURBO_LUAROCKS_BIN=%TURBO_LUAROCKS%\2.2
+SET TURBO_ROCKS=%TURBO_ROOT%\rocks
+SET TURBO_SRC=%TURBO_ROOT%\src
+SET TURBO_OPENSSL_ROOT=%TURBO_ROOT%\OpenSSL
+SET VCVARSALL_BAT="C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat"
+
+setx PATH "%PATH%;%TURBO_ROOT%;%TURBO_LUAROCKS_BIN" /M
 @powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
-choco install -y visualstudio2013professional git mingw openssl.light gnuwin
-SET PATH=%PATH%;C:\Program Files (x86)\Git\cmd;C:\GnuWin\bin;C:\tools\mingw64\bin;C:\Program Files (x86)\Windows Kits\8.1\bin\x64;C:\turbo.lua;C:\turbo.lua\luarocks\2.2
-SET VS120COMNTOOLS=C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\Tools\
-rm -rf C:\turbo.lua
-mkdir C:\turbo.lua
-cd C:\turbo.lua
+choco install -y visualstudio2013professional git mingw gnuwin strawberryperl
+call %VCVARSALL% amd64
+SET PATH=%PATH%;%TURBO_ROOT%;%TURBO_LUAROCKS_BIN;C:\Program Files (x86)\Git\cmd;C:\GnuWin\bin;C:\tools\mingw64\bin;C:\Program Files (x86)\Windows Kits\8.1\bin\x64
+rm -rf %TURBO_ROOT%
+mkdir %TURBO_ROOT%
+cd %TURBO_ROOT%
 mkdir src
 cd src
 
 git clone https://github.com/kernelsauce/turbo.git
-cd C:\turbo.lua\src\turbo
+cd %TURBO_SRC%\turbo
 mingw32-make SSL=none
 mv libtffi_wrap.so libtffi_wrap.dll
-setx TURBO_LIBTFFI "C:\turbo.lua\src\turbo\libtffi_wrap.dll" /M
-setx TURBO_LIBSSL "C:\Program Files\OpenSSL\libeay32.dll" /M
-SET TURBO_LIBTFFI=C:\turbo.lua\src\turbo\libtffi_wrap.dll
-SET TURBO_LIBSSL=C:\Program Files\OpenSSL\libeay32.dll
+setx TURBO_LIBTFFI "%TURBO_SRC%\turbo\libtffi_wrap.dll" /M
+setx TURBO_LIBSSL "%TURBO_OPENSSL_ROOT%\lib\libeay32.dll" /M
+SET TURBO_LIBTFFI=%TURBO_SRC%\turbo\libtffi_wrap.dll
+SET TURBO_LIBSSL=%TURBO_OPENSSL_ROOT%\lib\libeay32.dll
 
+cd %TURBO_SRC%
+wget DownloadFile https://www.openssl.org/source/openssl-1.0.2c.tar.gz
+7z x openssl-1.0.2c.tar.gz
+7z x openssl-1.0.2c.tar
+cd openssl-1.0.2c
+perl Configure VC-WIN64A --prefix=%OPENSSL_ROOT%
+ms\do_win64a
+nmake -f ms\nt.mak
+nmake -f ms\nt.mak install
 
-cd C:\turbo.lua\src
+cd %TURBO_SRC%
 git clone http://luajit.org/git/luajit-2.0.git
-cd C:\turbo.lua\src\luajit-2.0\src
-call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" amd64
+cd %TURBO_SRC%\luajit-2.0\src
 call msvcbuild
-cp luajit.exe C:\turbo.lua
-cp lua51.dll C:\turbo.lua
+cp luajit.exe %TURBO_ROOT%
+cp lua51.dll %TURBO_ROOT%
 
-cd C:\turbo.lua\src
+cd %TURBO_SRC%
 git clone https://github.com/keplerproject/luarocks.git
-cd C:\turbo.lua\src\luarocks
-SET LUA_PATH=C:\turbo.lua\rocks\share\lua\5.1\?.lua;C:\turbo.lua\rocks\share\lua\5.1\?\init.lua;C:\turbo.lua\src\turbo\?.lua;C:\turbo.lua\src\turbo\turbo\?.lua
-SET LUA_CPATH=C:\turbo.lua\rocks\lib\lua\5.1\?.dll
+cd %TURBO_SRC%\src\luarocks
+SET LUA_PATH=%TURBO_ROCKS%\share\lua\5.1\?.lua;%TURBO_ROCKS%\share\lua\5.1\?\init.lua;%TURBO_SRC%\turbo\?.lua;%TURBO_SRC%\turbo\turbo\?.lua
+SET LUA_CPATH=%TURBO_ROCKS%\lib\lua\5.1\?.dll
 setx LUA_PATH "%LUA_PATH%" /M
 setx LUA_CPATH "%LUA_CPATH%" /M
-call install.bat /TREE C:\turbo.lua\rocks\ /P C:\turbo.lua\luarocks\ /INC C:\turbo.lua\src\luajit-2.0\src /LIB C:\turbo.lua\src\luajit-2.0\src /LUA C:\turbo.lua /Q
+call install.bat /TREE %TURBO_ROCKS% /P %TURBO_LUAROCKS% /INC %TURBO_SRC%\luajit-2.0\src /LIB %TURBO_SRC%\luajit-2.0\src /LUA %TURBO_ROOT% /Q
 call luarocks install luasocket
 call luarocks install luafilesystem
 
